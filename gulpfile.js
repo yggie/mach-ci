55dd478a1833888edd8d255bc1ef5cc085d636d4
Bryan Yap <bryan.yap@alliants.com>
2015-03-08 02:08:46 +0000

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,59 @@
+(function (require) {
+  'use strict';
+
+  var gulp = require('gulp'),
+      usemin = require('gulp-usemin'),
+      sourcemaps = require('gulp-sourcemaps'),
+      babel = require('gulp-babel'),
+      concat = require('gulp-concat'),
+      less = require('gulp-less'),
+      config = {
+        paths: {
+          src: './src',
+          styles: './styles',
+          dist: './dist',
+        },
+      };
+
+
+  gulp.task('compile-less', function () {
+    return gulp.src(config.paths.styles + '/**/*.less')
+      .pipe(sourcemaps.init())
+      .pipe(less())
+      .pipe(concat('app.css'))
+      .pipe(sourcemaps.write('.'))
+      .pipe(gulp.dest(config.paths.dist));
+  });
+
+
+  gulp.task('compile-js', function () {
+    return gulp.src(config.paths.src + '/**/*.js')
+      .pipe(sourcemaps.init())
+      .pipe(babel())
+      .pipe(concat('app.js'))
+      .pipe(sourcemaps.write('.'))
+      .pipe(gulp.dest(config.paths.dist));
+  });
+
+
+  gulp.task('compile-all', ['compile-less', 'compile-js']);
+
+
+  gulp.task('build', ['compile-all'], function () {
+    return gulp.src('./index.html')
+      .pipe(usemin())
+      .pipe(gulp.dest('dist/'));
+  });
+
+
+  gulp.task('watch', function () {
+    gulp.watch([config.paths.src + '/**/*.js'], ['compile-js']);
+    gulp.watch([config.paths.styles + '/**/*.less'], ['compile-less']);
+  });
+
+
+  gulp.task('default', ['compile-all'], function () {
+    gulp.start('watch');
+  });
+
+}).call(this, require);
