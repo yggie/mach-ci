05152e802d2c23aede8f94e53717e236c82289e1
Bryan Yap <bryan.yap@alliants.com>
2015-09-12 18:59:13 +0100

    Add support for rendering TriangleMeshes (Body)
    - Bumped version of mach-test-browser-standalone

--- client/utils.js
+++ client/utils.js
@@ -35,6 +35,25 @@ export const parseGeometry = function (string) {
     let radius = parseFloat(match[1]);
 
     return new THREE.SphereGeometry(radius);
+  } else if (match = string.match(/TriangleMesh{(.+)}/)) {
+    let i = 0;
+    let geometry = new THREE.Geometry();
+
+    let m = null;
+    let vertices = geometry.vertices;
+    let regex= /(\[-?\d+(?:\.\d+)?, -?\d+(?:\.\d+)?, -?\d+(?:\.\d+)?\])/g;
+    while (m = regex.exec(match[1])) {
+      let vertex = parseVector(m[1]);
+      vertices.push(vertex);
+    }
+
+    for (i = 0; i < vertices.length; i += 3) {
+      geometry.faces.push(new THREE.Face3(i, i + 1, i + 2));
+    }
+
+    geometry.computeFaceNormals();
+
+    return geometry;
   }
 
   throw 'Unable to parse shape from string: "' + string + '"';
